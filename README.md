17bcbce12ce5e0a34ff2867187f020898395aea6
Enrique Casanova <enrique.casanova@projekt202.com>
2022-12-06 14:19:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# OpenAI Slack Bot
+
+You need to register an app with Slack. To do this, visit: https://api.slack.com/apps/
+
+You also need to register personal OpenAI API-Keys, visit: https://beta.openai.com/account/api-keys
+
+## Permissions Scopes
+
+You must configure the following permissions in your Slack App and generate the OAuth tokens
+
+```
+app_mentions:read
+im:read
+im:write
+mpim:read
+mpim:write
+```
+
+You also need setup socket mode in your app
+
+## Enviroment variables
+
+You must set the environment variables
+
+```
+OPENAI_API_KEY=
+OPENAI_ORG_ID=
+SLACK_SIGNING_SECRET=
+SLACK_OAUTH_TOKEN=
+SLACK_APP_TOKEN=
+```
+
+## Run the app
+
+```
+npm start
+```
---
+++ index.js
@@ -0,0 +1,43 @@
+import SlackBot from "@slack/bolt";
+import dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+const { App } = SlackBot;
+
+dotenv.config();
+
+const app = new App({
+  token: process.env.SLACK_OAUTH_TOKEN, //Find in the Oauth  & Permissions tab
+  signingSecret: process.env.SLACK_SIGNING_SECRET, // Find in Basic Information Tab
+  appToken: process.env.SLACK_APP_TOKEN, // Token from the App-level Token that we created
+  socketMode: true,
+});
+
+const openAiConfiguration = new Configuration({
+  organization: process.env.OPENAI_ORG_ID,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(openAiConfiguration);
+
+app.event("app_mention", async ({ event, say }) => {
+  try {
+    let query = event.text.substr(event.text.indexOf(" ") + 1);
+    const rsp = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: query,
+      temperature: 0.5,
+      max_tokens: 1000,
+      top_p: 1.0,
+      frequency_penalty: 0.5,
+      presence_penalty: 0.0,
+      stop: ["You:"],
+    });
+    console.log(rsp.data);
+    //let rspChat = rsp.data.choices[0].text.replace(/[\r\n]/gm, "");
+    //await say(`<@${event.user}>: ${rspChat}`);
+  } catch (error) {
+    console.error(error);
+  }
+});
+app.start(3000);
